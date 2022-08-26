c99c9fd8b360f87af5e53bd3c105695efd222fce
David Mora <dsmorat14@gmail.com>
2022-08-26 13:57:25 +0200

    feat: adapting to me
    Signed-off-by: David Mora <dsmorat14@gmail.com>

--- components/Layout.js
+++ components/Layout.js
@@ -16,14 +16,8 @@ export function GradientBackground({ variant, className }) {
 
 export default function Layout({ children }) {
   const setAppTheme = () => {
-    const darkMode = localStorage.getItem('theme') === 'dark';
-    const lightMode = localStorage.getItem('theme') === 'light';
-
-    if (darkMode) {
-      document.documentElement.classList.add('dark');
-    } else if (lightMode) {
-      document.documentElement.classList.remove('dark');
-    }
+    const darkMode = 'dark';
+    document.documentElement.classList.add('dark');
     return;
   };
 
@@ -50,7 +44,7 @@ export default function Layout({ children }) {
   }, []);
 
   return (
-    <div className="relative pb-24 overflow-hidden">
+    <div>
       <div className="flex flex-col items-center max-w-2xl w-full mx-auto">
         {children}
       </div>
